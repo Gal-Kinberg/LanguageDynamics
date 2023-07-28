4d83cdcc4ac970292d0332f1f4a3910a634078c8
Gal Kinberg <Gankl33@gmail.com>
2023-07-28 19:39:54 +0300

    First Attempts
    preliminary tests.
    learn to use the networks and generate text, and get the embeddings of the results.

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from transformers import pipeline, GPT2Tokenizer, GPT2LMHeadModel, AutoTokenizer, AutoModelForCausalLM
+from torch import Tensor
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    model_name = "gpt2"
+    top_k = 1
+    task = "text-generation"
+    max_new_tokens = 30
+    num_return_sequences = 1
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
+    tokenizer.pad_token = tokenizer.eos_token
+    model = GPT2LMHeadModel.from_pretrained('gpt2', output_hidden_states=True)
+
+    prompt = "Man King Woman Queen Box bird planet democracy"
+
+    encoded_input = tokenizer(prompt,
+                              return_tensors='pt',
+                              max_length=100,
+                              padding=True,
+                              truncation=True)
+    output = model.generate(**encoded_input, max_new_tokens=max_new_tokens)
+    print(tokenizer.batch_decode(output, skip_special_tokens=True))
+    embeds = model.transformer.wte(output)
+    # generator = pipeline(task=task, model=model_name)
+
+    # result = generator(prompt, max_new_tokens=2)
+    # print(result)
+
+    # TODO: input sentence to network, get output, pad tokens to fixed size, get embeddings
