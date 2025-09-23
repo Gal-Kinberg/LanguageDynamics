cfb671890bd3a8edd15d5173d12bbe60c71b46d0
Gal-Kinberg <Gankl33@gmail.com>
2025-09-23 18:48:32 +0300

    added TrajectoryDataset

--- src/data_generation/__init__.py
+++ src/data_generation/__init__.py
@@ -1,4 +1,4 @@
 from .dyck_data_generation import *
 from .dyck_tokenization import *
-from .tokens_dataset import TokensDataset, TokensAutoencoderDataset
+from .tokens_dataset import TokensDataset, TokensAutoencoderDataset, TrajectoryDataset
 from .tiny_memory_generation import *
--- src/data_generation/tokens_dataset.py
+++ src/data_generation/tokens_dataset.py
@@ -25,3 +25,13 @@ class TokensAutoencoderDataset(Dataset):
         x = self.data[idx]
         y = self.data[idx]
         return x, y
+
+class TrajectoryDataset(Dataset):
+    def __init__(self, dataset):
+        self.dataset = dataset
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, idx):
+        return self.dataset[idx]
